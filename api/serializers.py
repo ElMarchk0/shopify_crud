eff7bb709771e4bf43284ea319c4110f387693c8
ElMarchk0 <67571255+ElMarchk0@users.noreply.github.com>
2022-01-15 14:20:50 -0800

    a few more edits

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 #From app
 from api.models import Product
 
+
+#Serializers are used to render json and xml data
 class ProductSerializer(serializers.ModelSerializer):
   class Meta:
     model = Product
